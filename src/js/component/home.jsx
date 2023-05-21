f269bf887e59eccdaf9dfd320f3835d8bf36e29f
Eugenia Rivero <126787554+eugenia3031@users.noreply.github.com>
2023-05-21 15:37:37 +0000

    last commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("")
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="caja-1">
+			<div className={color === "red" ? "red on" : "red"} onClick={() => setColor("red")}></div>
+			<div className={color === "yellow" ? "yellow on" : "yellow"} onClick={() => setColor("yellow")}></div>
+			<div className={color === "green" ? "green on" : "green"} onClick={() => setColor("green")}></div>
 		</div>
+		<div className="d-grid gap-2 col-2 mx-auto">
+			<button type="button" class="btn btn-dark m-3" onClick={(event) => {
+				if(color === "red"){
+					setColor("yellow");
+				} else if(color === "yellow"){
+					setColor("green");
+				} else if(color === "green"){
+					setColor("red");
+				}
+			}}>Click me to change</button>
+		</div>
+		
+		</>
 	);
 };
 
